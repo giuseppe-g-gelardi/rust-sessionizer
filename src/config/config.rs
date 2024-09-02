41f92c4e357f3daeaaf62d7b905250c9131fafee
giuseppe-g-gelardi <giuseppegelardi@icloud.com>
2024-09-01 22:22:41 -0400

    narrowing down cfg manager and config implementations. narrowing down control flow

--- src/config/config.rs
+++ src/config/config.rs
@@ -1,29 +1,31 @@
 use serde::Deserialize;
 use serde_json::json;
 
-// use serde_json::from_str;
-//
 use std::fs;
 use std::io::Error;
 
+use dirs;
+use std::env;
+use std::path::PathBuf;
+
 #[derive(Debug, Deserialize)]
-pub struct Config { // should specify if this is json?
+pub struct Config {
+    // should specify if this is json?
     pub access_token: String,
     pub editor: String,        // vscode, neovim, vim, emacs, etc.
     pub alias: Option<String>, // user defined alias
     pub tmux: bool,
 }
 
-struct ConfigManager {
-    config_file_name: String,
-    default_config: Config,
+#[derive(Debug)]
+pub struct ConfigManager {
+    pub config_file_name: String,
+    pub default_config: Config,
 }
 
 impl ConfigManager {
     pub fn new() -> Self {
-        // should name this new_config_manager to not confuse with
-        // Config::new()???
-        let config_file_name = "config.json".to_string(); // getConfigFileLocation()
+        let config_file_name = "cfg.json".to_string(); // getConfigFileLocation()
         let mut default_config = Config::new();
 
         default_config.access_token = "".to_string();
@@ -34,6 +36,50 @@ impl ConfigManager {
         Self {
             config_file_name,
             default_config,
+        }
+    }
+
+    pub fn get_config_file_location(&self) -> Option<PathBuf> {
+        let home_dir = dirs::home_dir()?; // Get user's home directory
+
+        // Determine config directory based on operating system
+        let config_dir = match std::env::consts::OS {
+            "windows" => {
+                // Get APPDATA environment variable for Windows
+                let appdata = env::var("APPDATA").ok()?;
+                PathBuf::from(appdata).join("local")
+            }
+            "linux" | "macos" => {
+                // Use user's home directory and append .config for Linux and macOS
+                home_dir.join(".config")
+            }
+            _ => {
+                eprintln!("Unsupported operating system");
+                return None;
+            }
+        };
+
+        // Create "cfg.json" path
+        Some(config_dir.join("cfg.json"))
+    }
+
+    pub fn get_config(&self, recursive_depth: u8) -> Result<Config, Error> {
+        if recursive_depth > 5 {
+            return Err(Error::new(std::io::ErrorKind::NotFound, "File not found"));
+        }
+
+        match fs::read_to_string(&self.config_file_name) {
+            Ok(content) => match serde_json::from_str::<Config>(&content) {
+                Ok(config) => Ok(config),
+                Err(e) => Err(Error::new(
+                    std::io::ErrorKind::InvalidData,
+                    "Invalid data, {}".to_string() + &e.to_string(),
+                )),
+            },
+            Err(_) => {
+                // Error reading config file, making a new one // self.write_config(None)?;
+                self.get_config(recursive_depth + 1)
+            }
         }
     }
 }
@@ -47,8 +93,6 @@ impl Config {
             tmux: false,
         }
     }
-
-    pub fn get_config(recursive_depth: u8) {}
 
     pub fn to_json(&self) -> serde_json::Value {
         json!({
@@ -75,20 +119,23 @@ impl Config {
         // let mut cfg = Config::new();
 
         self.access_token = "asdfasdf".to_string();
-        self.editor = "VSCode".to_string();
+        self.editor = "vscode".to_string();
         self.alias = None;
-        self.tmux = true;
+        self.tmux = false;
 
         self.save_to_file(&file_name).unwrap();
     }
 
+    // for some reason this is reading the whole config...
+    // really need to narrow down whats happening because
+    // it seems like i have 5 functions doing effectively the same thing
     pub fn verify_access_token(&self, file_name: &String) -> Result<(), Error> {
         // this next line is pissing me off. I don't know what to do with it
         // keeps crashing the program instead of returning an error and moving
         // on with the rest of the program to circumvent the absence of the file
         // it is looking for.
         let text = fs::read_to_string(file_name);
-        println!("text: {:#?}", &text);
+        println!("!!!!!!!!!text: {:#?}", &text);
 
         // not sure what to do in here
 
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 // internal
-use config::config::Config;
-
-// crates
-// use serde_json::from_str;
+use config::config::{Config, ConfigManager};
 
 // standard library
 use std::fs;
@@ -12,127 +9,84 @@ use std::path::Path;
 // modules
 mod config;
 
-// help me
-use std::env;
-// use std::fs;
-use std::path::PathBuf;
-// use std::os::unix::fs::MetadataExt; // For Linux-specific extensions
-use dirs; // `dirs` crate helps in fetching the user's home directory and application data
-
+// constants ?
 const FILE_NAME: &str = "cfg.json";
 
+// ************************************************************************** //
+// how the control flow should work:
+// main fn
+// - instante the config manager (ConfigManager::new())
+// - - check if the config file exists (config_manager.get_config_file_location())
+// - - - if it does, verify the access token (if access_token == "") authenticate()
+// - - - if it doesn't, create a new config file (let mut cfg = Config::new())
+// - - - - update the config file with default values
+// - - - - save the config file
+//
+// - if the config file exists and the access token is verified ..
+// - - start the TUI application
+// ************************************************************************** //
+
 fn main() -> Result<(), Error> {
-    let loc = get_config_file_location();
-    match loc {
-        Some(path) => println!("Config file location: {:?}", path),
-        None => eprintln!("Failed to get config file location"),
-    }
+    let config_manager = ConfigManager::new();
+    let config = &config_manager.get_config(1);
+
+    println!("Config: {:?}", config);
+
+    
+
+    // let file_location = config_manager.get_config_file_location();
+    // match file_location {
+    //     //
+    //     Some(path) => println!("Config file location: {:?}", path),
+    //     None => eprintln!("Failed to get config file location"),
+    // }
 
     // let text = fs::read_to_string(file_name.to_string())?;
     let file_name: Vec<String> = std::env::args().collect();
-    let file_exists = verify_config_exists(&file_name[1]);
 
-    let mut cfg = Config::new();
+    //
+    //
+    //
+    // let file_exists = verify_config_exists(&file_name[1]);
 
-    cfg.update_config_file(&FILE_NAME.to_string()); // update_config_file();
+    // this should probably happen in the ConfigManager
+    // let mut cfg = Config::new();
 
-    verify_file_exists(&cfg, file_name, file_exists)?; // verify_file_exists(&cfg, file_name,
-                                                       // file_exists);
+    // cfg.update_config_file(&FILE_NAME.to_string()); // update_config_file();
 
-    // match file_exists {
-    //     Ok(_) => {
-    //         println!("file exists");
-    //         cfg.verify_access_token(&file_name[1]).unwrap();
-    //     }
-    //     Err(e) => {
-    //         println!(
-    //             "file does not exist, creating new config file. {:?}",
-    //             e.to_string()
-    //         );
-    //         cfg.create_config(&FILE_NAME.to_string()).unwrap();
-    //     }
-    // }
+    // verify_file_exists(&cfg, file_name, file_exists)?; // verify_file_exists(&cfg, file_name,
 
     Ok(())
 }
 
-pub fn get_config_file_location() -> Option<PathBuf> {
-    let home_dir = dirs::home_dir()?; // Get user's home directory
-
-    // Determine config directory based on operating system
-    let config_dir = match std::env::consts::OS {
-        "windows" => {
-            // Get APPDATA environment variable for Windows
-            let appdata = env::var("APPDATA").ok()?;
-            PathBuf::from(appdata).join("local")
-        }
-        "linux" | "macos" => {
-            // Use user's home directory and append .config for Linux and macOS
-            home_dir.join(".config")
-        }
-        _ => {
-            eprintln!("Unsupported operating system");
-            return None;
-        }
-    };
-
-    // Create "session_config.yaml" path
-    Some(config_dir.join("session_config.yaml"))
-}
-
-fn verify_file_exists(
-    cfg: &Config,
-    file_name: Vec<String>,
-    file_exists: Result<(), Error>,
-) -> Result<(), Error> {
-    match file_exists {
-        Ok(_) => {
-            println!("file exists");
-            cfg.verify_access_token(&file_name[1]).unwrap();
-        }
-        Err(e) => {
-            println!(
-                "file does not exist, creating new config file. {:?}",
-                e.to_string()
-            );
-            cfg.create_config(&FILE_NAME.to_string()).unwrap();
-        }
-    }
-
-    Ok(())
-}
-
-fn verify_config_exists(file_name: &String) -> Result<(), Error> {
-    if Path::new(file_name).exists() {
-        fs::read_to_string(file_name)?; // let text = ????? // need to get rid of this ? lol
-        Ok(())
-    } else {
-        Err(Error::new(std::io::ErrorKind::NotFound, "File not found"))
-    }
-}
-
-// fn create_config(file_name: &String) -> Result<(), Error> {
-//     let cfg = Config::new();
-//     cfg.save_to_file(file_name)?;
+// fn verify_file_exists(
+//     cfg: &Config,
+//     file_name: Vec<String>,
+//     file_exists: Result<(), Error>,
+// ) -> Result<(), Error> {
+//     match file_exists {
+//         Ok(_) => {
+//             println!("file existsIN VERIFY_FILE_EXISTS!!!!!!");
+//             cfg.verify_access_token(&file_name[1]).unwrap();
+//         }
+//         Err(e) => {
+//             println!(
+//                 "file does not exist, creating new config file. {:?}",
+//                 e.to_string()
+//             );
+//             cfg.create_config(&FILE_NAME.to_string()).unwrap();
+//         }
+//     }
+//
 //     Ok(())
 // }
-
-// fn verify_access_token(file_name: &String) -> Result<(), Error> {
-//     let text = fs::read_to_string(file_name.to_string())?;
 //
-//     let cfg: Config = from_str(&text)?;
-//
-//     println!("access_token: {}", cfg.access_token);
-//     Ok(())
-// }
-
-// fn update_config_file() {
-//     let mut cfg = Config::new();
-//
-//     cfg.access_token = "1234567890".to_string();
-//     cfg.editor = "VSCode".to_string();
-//     cfg.alias = None;
-//     cfg.tmux = true;
-//
-//     cfg.save_to_file("cfg.json").unwrap();
+// fn verify_config_exists(file_name: &String) -> Result<(), Error> {
+//     if Path::new(file_name).exists() {
+//         let x = fs::read_to_string(file_name)?; // let text = ????? // need to get rid of this ? lol
+//         println!("xxxxxxx: {:?}", x);
+//         Ok(())
+//     } else {
+//         Err(Error::new(std::io::ErrorKind::NotFound, "File not found"))
+//     }
 // }
