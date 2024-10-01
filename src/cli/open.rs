a82b1bfd6d221a57004bbe9b143b3707beff322d
giuseppe-g-gelardi <giuseppegelardi@icloud.com>
2024-09-30 20:56:28 -0400

    .

--- src/cli/open.rs
+++ src/cli/open.rs
@@ -1,4 +1,4 @@
-use crate::cli::cli::init;
+// use crate::cli::cli::init;
 use crate::config::config::CfgManager;
 use crate::repo::repo::PartialRepo;
 
@@ -18,7 +18,33 @@ pub fn open(cm: &CfgManager, repos: Vec<PartialRepo>) {
         .unwrap();
 
     let selected_repo = &repos[selections].clone(); // -> gets passed to the next function
-    println!("Opening repo: {:#?}", selected_repo);
+    // println!("Opening repo: {:#?}", selected_repo);
 
-    init(cm, repos)
+    // init(cm, repos)
+    // NOTE: this shouldnt go back to init, 
+    //
+    // the next step is to have prompts for: 
+    // - bare repo, 
+    // - html/ssh, 
+    // - and start the session
+    next_step(cm, selected_repo);
+}
+
+fn next_step(cm: &CfgManager, selected_repo: &PartialRepo) {
+    println!("Selected repo: {:#?}", selected_repo);
+    println!("user config: {:#?}", cm.get_config(1).unwrap());
+    // let choices = vec!["Bare Repo", "HTML/SSH"];
+    // let selections = Select::new()
+    //     .items(&choices)
+    //     .default(0)
+    //     .with_prompt("Select a repository to open")
+    //     .interact()
+    //     .unwrap();
+    //
+    // match selections {
+    //     0 => open_bare_repo(cm, selected_repo),
+    //     1 => open_html_ssh(cm, selected_repo),
+    //     _ => exit(),
+    // };
+
 }
