82c13aa2ee5c7cbad33cd7f8d1e3d35ed68af5e5
giuseppe-g-gelardi <giuseppegelardi@icloud.com>
2024-09-30 21:45:08 -0400

    start set up functions to build commands

--- src/cli/open.rs
+++ src/cli/open.rs
@@ -1,7 +1,8 @@
-use crate::config::config::CfgManager;
+use crate::config::config::{Cfg, CfgManager};
 use crate::repo::repo::PartialRepo;
 
 use dialoguer::Select;
+use std::error::Error;
 
 pub fn open(cm: &CfgManager, repos: Vec<PartialRepo>) {
     let repo_select_options = repos
@@ -17,32 +18,53 @@ pub fn open(cm: &CfgManager, repos: Vec<PartialRepo>) {
         .unwrap();
 
     let selected_repo = &repos[selections].clone(); // -> gets passed to the next function
-    // println!("Opening repo: {:#?}", selected_repo);
+                                                    // println!("Opening repo: {:#?}", selected_repo);
 
-    // NOTE: this shouldnt go back to init, 
-    //
-    // the next step is to have prompts for: 
-    // - bare repo,
-    // - html/ssh, 
-    // - and start the session
-    next_step(cm, selected_repo);
-}
-
-fn next_step(cm: &CfgManager, selected_repo: &PartialRepo) {
     println!("Selected repo: {:#?}", selected_repo);
     println!("user config: {:#?}", cm.get_config(1).unwrap());
-    // let choices = vec!["Bare Repo", "HTML/SSH"];
-    // let selections = Select::new()
-    //     .items(&choices)
-    //     .default(0)
-    //     .with_prompt("Select a repository to open")
-    //     .interact()
-    //     .unwrap();
-    //
-    // match selections {
-    //     0 => open_bare_repo(cm, selected_repo),
-    //     1 => open_html_ssh(cm, selected_repo),
-    //     _ => exit(),
-    // };
 
+    let config = cm.get_config(1).unwrap();
+    let editor_command = set_editor_command(&config);
+    let repo_url = set_repo_url(selected_repo);
+    let bare_repo = is_bare_repo();
+}
+
+fn command_builder(repo_url: &str, is_bare: bool) -> Vec<&str> {
+    let cmd = if is_bare {
+        vec!["git", "clone", "--bare", repo_url]
+    } else {
+        vec!["git", "clone", repo_url]
+    };
+    cmd
+}
+
+fn set_editor_command(config: &Cfg) -> String {
+    /*
+     * if alias is set, for example "c" for vscode, return alias
+     * else return editor. for example "neovim" // so the command to open will be "neovim ."
+     */
+    if config.alias.is_some() && config.alias.as_ref().unwrap() != "" {
+        return config.alias.as_ref().unwrap().to_string();
+    }
+    config.editor.to_string()
+}
+
+fn set_repo_url(repo: &PartialRepo) -> String {
+    // result of user prompt to clone via "html" or "ssh"
+    let repo_url = ""; // html_ssh() prompt
+
+    if repo_url == "ssh" {
+        return repo.ssh_url.to_string();
+    }
+    repo.html_url.to_string()
+}
+
+fn is_bare_repo() -> bool {
+    // result of user prompt to clone via "bare" or "html"
+    let bare_repo = ""; // bare_html() prompt
+
+    if bare_repo == "bare" {
+        return true;
+    }
+    false
 }
